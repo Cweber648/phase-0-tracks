8fe702387ece4d7ab64a658a1f7ddd72ba7731ec
Charlie Weber <Cweber648@gmail.com>
2017-06-19 17:30:46 -0500

    added text

---
+++ ruby/hamsters.rb
@@ -0,0 +1,9 @@
+puts "What is the hamsters name"
+name = gets.chomp
+puts "Hello #{name}, Glad to have you, What is your volume level"
+volume = gets.chomp
+puts "Thank you, your volume has been put as #{volume}"
+puts "What is your fur color"
+fur = gets.chomp
+
+
