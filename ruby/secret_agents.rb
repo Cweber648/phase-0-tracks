c7d337da3306e7346c4977d083a510a460339a65
Charlie Weber <Cweber648@gmail.com>
2017-06-25 19:55:19 -0700

    added decrypt

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -21,3 +21,24 @@ def encrypt(secret_message)
 end
 encrypt("abc")
 
+ def decrypt(secret_message)
+  letter_index = 0
+  alpha = "abcdefghijklmnopqrstuvwxyz"
+  new_message = ""
+
+  while letter_index < secret_message.length
+     /# figure out the index #/
+    if secret_message[letter_index]== " "
+      p secret_message[letter_index]
+    else
+      p secret_message[letter_index]
+      alpha_index = alpha.index(secret_message[letter_index])
+      alpha_index = alpha_index - 1
+      new_message += alpha[alpha_index]
+      letter_index +=1
+
+    end
+  end
+      puts "#{new_message}"
+end
+decrypt("abc")
