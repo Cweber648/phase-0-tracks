3994b72af428c44ef9bc17d36ba223f4a9c460d7
Charlie Weber <Cweber648@gmail.com>
2017-07-01 21:09:24 -0500

    inital nested data

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,38 @@
+wrigley_field {
+
+  park_info: {
+    seats: 45,000,
+    teams: 1,
+
+     team_nicknames: [
+     'The North-siders'
+     'Cubbies'
+     'The Loveable Loser'
+     ]
+  },
+  parts_of_stadium: {
+    bleachers:6000
+    rest_stadium: 39,000,
+      inside_park: [
+       'Infield',
+       'Center Field',
+       'Right Field',
+       'Left Field',
+       'Press Box'
+    ]
+  }
+   on_field: {
+   bases:4 ,
+   postions:9 ,
+   players_per_team: 40,
+
+    some_positions: [
+      'shortstop',
+      'pitcher',
+      'catcher',
+      'Fans, they count as well',
+      'First Base'
+      ]
+
+  }
+}
