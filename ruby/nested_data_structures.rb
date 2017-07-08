bf61be2d4d7b4af02ea4866800e4050996991d75
Sang Wook Bok <sam.bok.km@gmail.com>
2017-07-08 16:36:26 -0400

    Add nested data structure of purple rain album

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,50 @@
+# Release 2
+# Nested data structure for R&B Albums chart on Billboard
+
+purple_rain = {
+  artists: ["Prince","The Revolution"],
+  release_date: "June 25, 1984",
+  genres: ["Rock", "Funk", "Pop", "Soul", "R&B"],
+  track: {
+    side_one: [
+      { no: 1,
+        title: "Let's Go Crazy",
+        length: "4:39"
+      },
+      { no: 2,
+        title: "Take Me with U",
+        length: "3:45"
+      },
+      { no: 3,
+        title: "The Beatiful Ones",
+        length: "5:13"
+      },
+      { no: 4,
+        title: "Computer Blue",
+        length: "3:50"
+      },
+      { no: 5,
+        title: "Darling Nikki",
+        length: "4:14"
+      }
+    ],
+    side_two: [
+      { no: 6,
+        title: "When Doves Cry",
+        length: "5:54"
+      },
+      { no: 7,
+        title: " I Would Die 4 U",
+        length: "2:49"
+      },
+      { no: 8,
+        title: "Baby I'm a Star",
+        length: "4:24"
+      },
+      { no: 9,
+        title: "Purple Rain",
+        length: "8:41"
+      } 
+    ]
+  }
+}
