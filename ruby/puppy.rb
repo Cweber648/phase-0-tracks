b679d868866bf46527a002eeb89a096cee26e314
Sang Wook Bok <sam.bok.km@gmail.com>
2017-07-09 20:06:27 -0400

    Add Puppy class design

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# Release 0: Design a Class
+# Puppy
+#   SPECIES-----------------
+#     Canis lupus familiaris
+#   CHARACTERISTICS---------
+#     Name: varies
+#     Breed: varies
+#     Age: varies
+#     Color: varies
+#   BEHAVIOR----------------
+#     Bark
+#     Jump
+#     Chew
+#     Dig
