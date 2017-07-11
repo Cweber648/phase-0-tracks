f25ee050d63e755ea28933ce8dbcfa3528ee2d8b
Sang Wook Bok <sam.bok.km@gmail.com>
2017-07-11 19:41:16 -0400

    Add more methods including getter and setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,6 +15,26 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}"
   end
+  
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(name)
+    @reindeer_ranking << @reindeer_ranking.delete_at(@reindeer_ranking.index(name))
+  end
+
+  def gender=(gender)
+    @gender = gender
+  end
+
+  def age
+    @age
+  end
+  
+  def ethnicity
+    @ethnicity
+  end
 
 end
 
@@ -24,9 +44,19 @@ end
 #santa.speak
 #santa.eat_milk_and_cookies("raison cookie")
 # Release 1
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+#santas = []
+#example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+#example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+#example_genders.length.times do |i|
+#  santas << Santa.new(example_genders[i], example_ethnicities[i])
+#end
+#santas.each do |santa|
+#  santa.speak
+#end
+# Release 2
+santa = Santa.new("male", "white", 68)
+santa.celebrate_birthday
+puts santa.age
+puts santa.ethnicity
+p santa.gender = "agender"
+p santa.get_mad_at("Dasher")
