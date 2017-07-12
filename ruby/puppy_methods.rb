ec5eae9bdc24237f9088ee2d7ce179d92909d76c
Charlie Weber <Cweber648@gmail.com>
2017-07-11 22:15:55 -0500

    added all methods for dogs

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,35 @@
 class Puppy
 
+
   def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+  @toy = toy
+  puts "I brought back the #{@toy}!"
+  toy
+  end
+
+  def speak(bark_times)
+    puts "Woof!" *(bark_times)
+  end
+
+  def roll_over
+    puts "roll over"
+  end
+
+  def dog_years(years)
+    dog_years = 0
+    @dog_years = dog_years
+    @dog_years = years * 7
+    puts "your dog is #{@dog_years}"
+  end
+
+  def sit
+    puts "good boy"
   end
 
 end
-
+puppy1 = Puppy.new
+puppy1.fetch('ball')
+puppy1.speak(5)
+puppy1.roll_over
+puppy1.dog_years(5)
+puppy1.sit
