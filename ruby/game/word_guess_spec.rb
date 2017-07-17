f07aef5c001e469ea49dc3342e42bf081e8074bf
Sang Wook Bok <sam.bok.km@gmail.com>
2017-07-16 21:23:55 -0400

    Add tests for word-guess class

--- ruby/game/word_guess_spec.rb
+++ ruby/game/word_guess_spec.rb
@@ -1,9 +1,18 @@
 require_relative "word_guess"
 
 describe WordGuess do
-  let(:word_guess) { WordGuess.new }
+  let(:word_guess) { WordGuess.new("word") }
 
-  it "creates a word or a phrase to play game" do
-    expect(word_guess.create_word_phrase("word")).to include(word_phrase: ["w","o","r","d"], secret: ["_","_","_","_"])
+  it "creates a word to play game" do
+    expect(word_guess.word).to eq(["W","O","R","D"])
+    expect(word_guess.secret_word).to eq(["_","_","_","_"])
+  end
+
+  it "updates number of guesses" do
+    expect(word_guess.chances).to eq(4)
+    word_guess.guess("a")
+    expect(word_guess.chances).to eq(3)
+    word_guess.guess("a")
+    expect(word_guess.chances).to eq(3)
   end
 end
