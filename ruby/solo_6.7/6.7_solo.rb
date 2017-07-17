801ab85d9d4a4c7f07f9764ff9aa4f11ec72e1ac
Charlie Weber <Cweber648@gmail.com>
2017-07-16 19:51:13 -0500

    reviewed rubric steps to see if code was complient with review

--- ruby/solo_6.7/6.7_solo.rb
+++ ruby/solo_6.7/6.7_solo.rb
@@ -1,12 +1,3 @@
-#Class (for our cup game) designed for a computer program
-#we want our driver code to be able to handle user input
-#gets.chomp
-
-#user enters a word and another user attempts to guess the word.
-#guesses are limited. Number of guess = the lenght of the word
-#The guessing player receives continual feedback on the current word. word is chocolate user guess as C it brings back C--c-----
-
-#class(Word_game)
 
 #def initialize
 #What are we intializing at the start of this game.
@@ -17,45 +8,90 @@
 #end
 
 class WordGame
-  #attr variables
-  attr_reader :word , :guess_count , :is_over , :word_array , :guess_word , :correct_letter
+
+#attr_accessor variables
+
+attr_accessor :word , :guess_count , :is_over , :word_array , :guess_word , :correct_letter, :word_length, :reused_letter
 
 #def initialize
 #What are we intializing at the start of this game.
-
+#word and guess count, the game is not over.
 #@guess_count = 0 (total guesses to start off are at 0)
 #@is_over = false. The game is not over until all guesses are exausted.
 #@word_count = what word we enter
 #end
-  def intialize
+  def intialize(word)
+    @word = word
     @guess_count = 0
     @is_over = false
-    @word = word
-    @word_array = "#{@word}".split('')
-    @guess_word = "#{@word}".split('')
     @correct_letter = 0
-
+    @word_length = @word.length
+    @word_array = "#{@word}".split('')
+    @reused_letter = false
   end
+
 #Write a method that will take the users word and then split it into characters to guess.
-  def user_word
-    @word = gets.chomp
-    @guess_word.map! do |replace_letter|
-    replace_letter = '-'
-    end
-    p guess_word.join("")
+#input = user1 puts in a word
+#output = the word replaced by - for its length
+
+#Write a method
+#input = letter and index
+#we are setting if the user guesses the word correct, we want to find the index of that word in the array and replace it with the letter.
+  def user_word(word)
+   @guess_word = "#{word}".split('')
+   @guess_word.map! do |replace_letter|
+      replace_letter = '-'
+   end
   end
 
-  def user_guess(letter,index)
-    @guess_count += 1
-    #guess_index = @word_array.index(guess_letter)
-    if @word_array[index] == letter
-      @guess_word[index] = letter
-      @correct_letter = 1
+#method for the user_guess
+#input - when the user inputs a correct letter. we want to replace the "- " with the correct letter at the index of the "-"
+
+  def user_guess(letter)
+   index = 0
+   while index < @word_length
+     if @word_array[index] == letter
+      if @guess_word[index] == "-"
+         @guess_word[index] = letter
+         @correct_letter = 1
+      else
+        @reused_letter = true
+      end
+    end
+    index += 1
+   end
+  end
+
+#Method needed for the correct guess
+#input will be a letter.
+# steps,  if the input is wrong we want to return Wrong. and have them input again
+#if it is correct. we want to correct word to replace the -
+#Method included for repeated guess.
+#reused letter = true print another message
+  def correct_guess
+    if @reused_letter == true
+      puts "You already guessed that letter"
+      @reused_letter = false
+    elsif @correct_letter == 0 && !is_over
+      puts "Wrong!, try again"
+    end
+    if !is_over
+      p guess_word.join("")
+    end
+    @correct_letter = 0
+  end
+
+#method for the repeated guess
+  def guess_amount
+    if @reused_letter == false
+       @guess_count += 1
     end
   end
-  #method check to see if the full word has been guessed
-  #input =
-  #if false. We are going to say is_over = true
+
+#method check to see if the full word has been guessed
+#if false. We are going to say is_over = true
+#steps- if statement if there are no more - in the word. the word was correctly guessed.
+#output- printing message and word
   def correct_word
     if !@guess_word.index('-')
       @is_over = true
@@ -63,52 +99,35 @@ class WordGame
     end
   end
 
-
+#method to define when the game is over
+#Steps- need and if statement, and is_over set to true
+#when the word is guessed wrong. guesses are larger than the word length. puts "bad message"
+  def game_over
+   if @guess_count >= @word_length && !@is_over
+      @is_over = true
+      puts "You lost, I can't believe you couldn't guess a word as easy as #{@word.upcase}!!!"
+   end
+  end
 
 end
 
-while !wordgame.is_over
+game = WordGame.new
+puts "Welcome to the letter game"
+puts "User 1 enter a word for User 2 to guess"
+word = gets.chomp
+game.intialize(word)
+game.user_word(word)
+
+while !game.is_over
   puts "Guess A Letter"
   guess_letter = gets.chomp
-  while passed_index < @word.length
-    passed_index = 0
-    user_guess(guess_letter,passed_index)
-    passed_index += 1
-  end
-  if @correct_letter == 0
-    puts "Wrong!, try again"
-  else
-    puts "#{@guess_word}"
-    @correct_letter = 0
-  end
-  if @guess_count > @word.length
-    @is_over = true
-  end
+  game.user_guess(guess_letter)
+  game.correct_word
+  game.guess_amount
+  game.game_over
+  game.correct_guess
 end
 
 
 
-
-
-#method change '-' in word to correct letters
-#input = the word the user put in for the other user to guess
-#output = if the user guesses right it will replace the - with the correct letter... is not right. print wrong guess again
-
-#We want a method that will take the word of the first user.
-
-
-
-
-# "Puts Welcome to the word guessing game"
-# game = WordGame.new
-
-# #while the game is not over. We want the
-# while !game.is_over
-#   puts "Guess a letter"
-#   guess = gets.chomp
-#   if !game.check_word[index]
-
-# puts "There is no #{letter} in this word "
-
-# puts "There is a #{letter} in the secret word"
 
