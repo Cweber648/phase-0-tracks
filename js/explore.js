c182ddb566f8b4ef504832f2d6b67658875a5f4c
Sang Wook Bok <sam.bok.km@gmail.com>
2017-07-17 19:41:22 -0400

    Add pseudocode for reverse string function

---
+++ js/explore.js
@@ -0,0 +1,21 @@
+/* Reverse function
+ * input: string
+ * steps:
+ *  split the string into letters
+ *  reverse the letters
+ *  concatenate the letters into string
+ * output:
+ *  reversed string
+ *
+ * OR
+ *
+ * input: string
+ * steps:
+ *  create a container for reversed string
+ *  For each letters from the last to the first
+ *    concatenate the letter to the reversed string container
+ * output:
+ *  reversed string
+ *    
+ *
+ */
