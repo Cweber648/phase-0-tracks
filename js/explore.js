56a8b0a1c8a7bec5bbf71c2b6b441c0c0937a643
Sang Wook Bok <sam.bok.km@gmail.com>
2017-07-17 19:50:23 -0400

    Add reverse string function and driver code

--- js/explore.js
+++ js/explore.js
@@ -16,6 +16,25 @@
  *    concatenate the letter to the reversed string container
  * output:
  *  reversed string
- *    
- *
  */
+
+/* Using Built-in Functions
+ * function reverse(string) {
+ *   return string.split("").reverse().join("");
+ * }
+ */
+
+// Reverse Function
+function reverse(string) {
+  var reverseString = "";
+  for (var i = string.length - 1; i >= 0; i--) {
+    reverseString += string[i]; 
+  }
+  return reverseString;
+}
+
+// DRIVER CODE
+var word = reverse("word") == "drow";
+console.log(word);
+var javaScript = reverse("JavaScript") == "tpircSavaJ";
+console.log(javaScript);
