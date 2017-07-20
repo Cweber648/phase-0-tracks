79cc1738bb3413ae920664c891d1257f82739512
Sang Wook Bok <sam.bok.km@gmail.com>
2017-07-19 21:01:39 -0400

    Add refactored methods

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -29,17 +29,17 @@ class VirusPredictor
   def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      ratio = 0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      ratio = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      ratio = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      ratio = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      ratio = 0.05
     end
-
+    number_of_deaths = (@population * ratio).floor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
@@ -50,19 +50,28 @@ class VirusPredictor
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
-    end
-
-    puts " and will spread across the state in #{speed} months.\n\n"
+    #if @population_density >= 200
+    #  speed += 0.5
+    #elsif @population_density >= 150
+    #  speed += 1
+    #elsif @population_density >= 100
+    #  speed += 1.5
+    #elsif @population_density >= 50
+    #  speed += 2
+    #else
+    #  speed += 2.5
+    #end
+    
+    # Loop 200 to 0 of decrement of 50
+    #density = 200
+    #5.times do
+    #  speed += 0.5
+    #  if @population_density >= density
+    #    spread = speed  
+    #  end 
+    #  density -= 50
+    #end
+    puts " and will spread across the state in #{spread} months.\n\n"
 
   end
 
