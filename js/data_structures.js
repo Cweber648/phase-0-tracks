82024dd18b6e06cc75feeea1e13f786e7b9b5503
Sang Wook Bok <sam.bok.km@gmail.com>
2017-07-20 20:07:37 -0400

    Add work for Release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,3 +2,9 @@ var colors = ["blue", "red", "yellow", "green"];
 var names = ["ed", "bob", "suzy", "jane"];
 colors.push("pink");
 names.push("joe");
+
+var horses = {}
+for (var i = 0; i < names.length; i++) {
+	horses[names[i]] = colors[i];
+}
+console.log(horses)
