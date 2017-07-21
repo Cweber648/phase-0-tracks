3edd5d837efd617520b98d67b2ed3db172c9a836
Sang Wook Bok <sam.bok.km@gmail.com>
2017-07-20 21:31:50 -0400

    Add pseudocode for find a key value match

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,10 @@
 // Find the Longest Phrase
+// input: list of words
+// steps:
+//  assign the first word to longest word
+//  until the end of the list,
+//  compare the length of the longest word to the length of next word
+//  assign the longer word to longest word 
 function longest(words) {
   var longest = words[0]; 
   for (var i = 1; i < words.length; i++) {
@@ -11,3 +17,11 @@ function longest(words) {
 
 // DRIVER CODE
 console.log(longest(["long phrase","longest phrase","longer phrase"]));
+
+// Find a Key-Value Match
+// input: two key-value pair
+// steps:
+//  get all the keys for first key-value pair
+//  check if second key-value pair has any key in the first one
+//  if there is a matching key, check if the they have same value
+// output: true or false
