75bd7eaf28bfc2aba4077f049e17610e46e68f85
Sang Wook Bok <sam.bok.km@gmail.com>
2017-08-02 13:33:57 -0400

    Add work from @Cweber648

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -25,3 +25,12 @@ post '/students' do
 end
 
 # add static resources
+get '/addresses' do
+  @addresses = {austin:"1705 Guadalupe Street, 1st Fl Austin, TX 78701", 
+                Chicago:"1033 W. Van Buren Street, 3rd Fl Chicago, IL 60607",
+                nyc:"48 Wall St, 15th Fl New York, NY 10005",
+                sd:"707 Broadway, Suite 800 San Diego, CA 92101",
+                sf:"633 Folsom Street (at Hawthorne), 6th Floor San Francisco CA 94107",
+                seattle:"83 S. King StreetSeattle, WA 98104"}
+  erb :addresses
+end
